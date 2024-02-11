f00d0daf5d898198b29a28fa9cf9d2609dda3ba9
aditya bhojane <adityabhojane66@gmail.com>
2024-02-11 18:32:20 +0530

    add color project level 3

--- projects/1 color changer/level 3/main.js
+++ projects/1 color changer/level 3/main.js
@@ -1,18 +1,28 @@
 // selecting button
 const colorChangerBtn = document.getElementById("btnColorChanger")
+const Stop = document.getElementById("Stop")
 // selecting span element
 const addColorCode = document.querySelector("span")
 
+
+// bar name
+const Red = document.querySelector("#red")
+
 // added event lister for btn
 colorChangerBtn.addEventListener("click",()=>{
-    // generating three number values form 0 to 255
-    const colorOne = Math.round(Math.random()*255)
-    const colorTwo = Math.round(Math.random()*255)
-    const colorThree = Math.round(Math.random()*255)
-    // combining color values into rbg format using string interpolation
-    const generatedColor = `rgb(${colorOne},${colorTwo},${colorThree})`
-    // pushing that rgb color and changing style
-    document.body.style.backgroundColor = generatedColor
-    // pushing rbg code in span 
-    addColorCode.innerHTML = generatedColor
+    const colorInterval =setInterval(() => {
+        // generating three number values form 0 to 255
+        let colorOne = Math.round(Math.random()*255);
+        let colorTwo = Math.round(Math.random()*255)
+        let colorThree = Math.round(Math.random()*255)
+        // combining color values into rbg format using string interpolation
+        const generatedColor = `rgb(${colorOne},${colorTwo},${colorThree})`
+        // pushing that rgb color and changing style
+        document.body.style.backgroundColor = generatedColor
+        // pushing rbg code in span 
+        addColorCode.innerHTML = generatedColor
+    }, 1000);
+    Stop.addEventListener("click",()=>{
+        clearInterval(colorInterval)
+    })
 })
