96e787bddb12a8cdb19c1c425c0a8273a3172aa8
aditya bhojane <adityabhojane66@gmail.com>
2024-04-24 13:50:39 +0530

    new ui added

--- projects/13-jokes/chaiaurcode.js
+++ projects/13-jokes/chaiaurcode.js
@@ -5,3 +5,13 @@ const url = 'https://api.chucknorris.io/jokes/random';
 // handle this end point with promises
 
 // handle the case of race condition
+
+gsap.from("div",{
+    y:300,
+    rotate:360,
+    duration:1,
+    backgroundColor:"black",
+    color:"yellow",
+    repeat:-1,
+    yoyo:true
+})
